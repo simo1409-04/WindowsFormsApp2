a84daca0ae250f25a56edafbc18d58c8c5ca168c
Simeon <simeon.ivanov2004@abv.bg>
2025-02-07 12:09:34 +0200

    Сложен е anchor

--- WindowsFormsApp2/FormEllipse.Designer.cs
+++ WindowsFormsApp2/FormEllipse.Designer.cs
@@ -45,9 +45,10 @@ namespace WindowsFormsApp2
             // button1
             // 
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(617, 395);
+            this.button1.Location = new System.Drawing.Point(823, 486);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(116, 23);
+            this.button1.Size = new System.Drawing.Size(155, 28);
             this.button1.TabIndex = 0;
             this.button1.Text = "Изчертаване";
             this.button1.UseVisualStyleBackColor = true;
@@ -55,16 +56,18 @@ namespace WindowsFormsApp2
             // 
             // numericUpDown1
             // 
-            this.numericUpDown1.Location = new System.Drawing.Point(568, 186);
+            this.numericUpDown1.Location = new System.Drawing.Point(757, 229);
+            this.numericUpDown1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.numericUpDown1.Name = "numericUpDown1";
-            this.numericUpDown1.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown1.Size = new System.Drawing.Size(160, 22);
             this.numericUpDown1.TabIndex = 2;
             // 
             // pictureBox1
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(22, 60);
+            this.pictureBox1.Location = new System.Drawing.Point(29, 74);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(534, 358);
+            this.pictureBox1.Size = new System.Drawing.Size(712, 441);
             this.pictureBox1.TabIndex = 3;
             this.pictureBox1.TabStop = false;
             // 
@@ -72,9 +75,10 @@ namespace WindowsFormsApp2
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(562, 78);
+            this.label2.Location = new System.Drawing.Point(749, 96);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(180, 32);
+            this.label2.Size = new System.Drawing.Size(233, 40);
             this.label2.TabIndex = 4;
             this.label2.Text = "Въведете радиус \r\nМаксимална стойност: 150";
             // 
@@ -82,26 +86,29 @@ namespace WindowsFormsApp2
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(315, 18);
+            this.label3.Location = new System.Drawing.Point(420, 22);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(245, 24);
+            this.label3.Size = new System.Drawing.Size(313, 29);
             this.label3.TabIndex = 5;
             this.label3.Text = "Изчертаване на Елипса";
             // 
             // numericUpDown2
             // 
-            this.numericUpDown2.Location = new System.Drawing.Point(568, 137);
+            this.numericUpDown2.Location = new System.Drawing.Point(757, 169);
+            this.numericUpDown2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.numericUpDown2.Name = "numericUpDown2";
-            this.numericUpDown2.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown2.Size = new System.Drawing.Size(160, 22);
             this.numericUpDown2.TabIndex = 6;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label1.Location = new System.Drawing.Point(565, 119);
+            this.label1.Location = new System.Drawing.Point(753, 146);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(62, 15);
+            this.label1.Size = new System.Drawing.Size(75, 18);
             this.label1.TabIndex = 7;
             this.label1.Text = "Височина";
             // 
@@ -109,17 +116,18 @@ namespace WindowsFormsApp2
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label4.Location = new System.Drawing.Point(565, 168);
+            this.label4.Location = new System.Drawing.Point(753, 207);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(53, 15);
+            this.label4.Size = new System.Drawing.Size(61, 18);
             this.label4.TabIndex = 8;
             this.label4.Text = "Ширина";
             // 
             // FormEllipse
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1119, 554);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.numericUpDown2);
@@ -128,6 +136,7 @@ namespace WindowsFormsApp2
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.numericUpDown1);
             this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "FormEllipse";
             this.Text = "Изчертаване на Елипса";
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
