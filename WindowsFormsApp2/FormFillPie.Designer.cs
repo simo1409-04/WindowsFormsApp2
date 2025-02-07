852740cd56d06aeedf80766f07fb72e72db51a21
Simeon <simeon.ivanov2004@abv.bg>
2025-02-07 13:40:11 +0200

    promeni po smenqne na background color na formata za zapylvane na paj

--- WindowsFormsApp2/FormFillPie.Designer.cs
+++ WindowsFormsApp2/FormFillPie.Designer.cs
@@ -47,9 +47,10 @@ namespace WindowsFormsApp2
             // button1
             // 
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(584, 272);
+            this.button1.Location = new System.Drawing.Point(779, 335);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(82, 23);
+            this.button1.Size = new System.Drawing.Size(109, 28);
             this.button1.TabIndex = 0;
             this.button1.Text = "Запълване";
             this.button1.UseVisualStyleBackColor = true;
@@ -57,9 +58,10 @@ namespace WindowsFormsApp2
             // 
             // pictureBox1
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(12, 61);
+            this.pictureBox1.Location = new System.Drawing.Point(16, 75);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(460, 361);
+            this.pictureBox1.Size = new System.Drawing.Size(613, 444);
             this.pictureBox1.TabIndex = 3;
             this.pictureBox1.TabStop = false;
             // 
@@ -67,9 +69,10 @@ namespace WindowsFormsApp2
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(223, 19);
+            this.label1.Location = new System.Drawing.Point(297, 23);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(189, 24);
+            this.label1.Size = new System.Drawing.Size(241, 29);
             this.label1.TabIndex = 10;
             this.label1.Text = "Запълване на Пай\r\n";
             // 
@@ -77,9 +80,10 @@ namespace WindowsFormsApp2
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(581, 206);
+            this.label4.Location = new System.Drawing.Point(775, 254);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(145, 26);
+            this.label4.Size = new System.Drawing.Size(186, 34);
             this.label4.TabIndex = 16;
             this.label4.Text = "Ъгъл на завъртане \r\nМаксимална стойност: 360\r\n";
             // 
@@ -87,9 +91,10 @@ namespace WindowsFormsApp2
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(581, 145);
+            this.label3.Location = new System.Drawing.Point(775, 178);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(145, 26);
+            this.label3.Size = new System.Drawing.Size(186, 34);
             this.label3.TabIndex = 15;
             this.label3.Text = "Начален ъгъл\r\nМаксимална стойност: 360\r\n";
             // 
@@ -97,38 +102,43 @@ namespace WindowsFormsApp2
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(581, 91);
+            this.label2.Location = new System.Drawing.Point(775, 112);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(145, 26);
+            this.label2.Size = new System.Drawing.Size(186, 34);
             this.label2.TabIndex = 14;
             this.label2.Text = "Радиус на окръжността: \r\nМаксимална стойност: 150";
             // 
             // numericUpDown3
             // 
-            this.numericUpDown3.Location = new System.Drawing.Point(584, 235);
+            this.numericUpDown3.Location = new System.Drawing.Point(779, 289);
+            this.numericUpDown3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.numericUpDown3.Name = "numericUpDown3";
-            this.numericUpDown3.Size = new System.Drawing.Size(138, 20);
+            this.numericUpDown3.Size = new System.Drawing.Size(184, 22);
             this.numericUpDown3.TabIndex = 13;
             // 
             // numericUpDown2
             // 
-            this.numericUpDown2.Location = new System.Drawing.Point(584, 178);
+            this.numericUpDown2.Location = new System.Drawing.Point(779, 219);
+            this.numericUpDown2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.numericUpDown2.Name = "numericUpDown2";
-            this.numericUpDown2.Size = new System.Drawing.Size(138, 20);
+            this.numericUpDown2.Size = new System.Drawing.Size(184, 22);
             this.numericUpDown2.TabIndex = 12;
             // 
             // numericUpDown1
             // 
-            this.numericUpDown1.Location = new System.Drawing.Point(584, 124);
+            this.numericUpDown1.Location = new System.Drawing.Point(779, 153);
+            this.numericUpDown1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.numericUpDown1.Name = "numericUpDown1";
-            this.numericUpDown1.Size = new System.Drawing.Size(142, 20);
+            this.numericUpDown1.Size = new System.Drawing.Size(189, 22);
             this.numericUpDown1.TabIndex = 11;
             // 
             // FormFillPie
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(1067, 554);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
@@ -138,6 +148,7 @@ namespace WindowsFormsApp2
             this.Controls.Add(this.label1);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "FormFillPie";
             this.Text = "Запълване на Пай";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
